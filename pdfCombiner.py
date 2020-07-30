87ac1699b0f6341039980de963e5ab3ee5d2df74
Ajay Jarhad <ajayjarhad1@gmail.com>
2020-07-31 03:43:20 +0530

    Final Commit

---
+++ pdfCombiner.py
@@ -0,0 +1,15 @@
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+
+def pdfCombiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('combined.pdf')
+
+
+pdfCombiner(inputs)
